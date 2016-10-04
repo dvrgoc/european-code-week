15e00dc6b3308cee37b201bd2141ffa717ab7343
Danijel Vrgoc <danijel.vrgoc@inchoo.net>
2016-10-04 22:48:08 +0200

    created index.php

---
+++ index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Homepage</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#">Categories</a></li>
+                <li><a href="#">Articles</a></li>
+                <li><a href="#">Search</a></li>
+            </ul>
+        </nav>
+    </header>
+    <sidebar>
+        <ul>
+            <li><p>Parent Category</p></li>
+            <li>
+                <p>Parent Category</p>
+                <ul>
+                    <li><p>Child Category</p></li>
+                    <li><p>Child Category</p></li>
+                </ul>
+            </li>
+        </ul>
+    </sidebar>
+    <main>
+        <section>
+            <p>It is October 19th, 2016, 10:29 AM</p>
+        </section>
+    </main>
+    <footer>
+        <p>European Code Week, Osijek, October 19th 2016</p>
+        <p><a href="mailto:danijel.vrgoc@inchoo.net">danijel.vrgoc@inchoo.net</a></p>
+    </footer>
+</body>
+</html>
