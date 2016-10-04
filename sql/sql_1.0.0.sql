3b1010f3825ddf60d7d892efb4b5ee5c6e14a340
Danijel Vrgoc <danijel.vrgoc@inchoo.net>
2016-10-04 22:33:43 +0200

    created tables and some basic data

--- sql/sql_1.0.0.sql
+++ sql/sql_1.0.0.sql
@@ -1,9 +1,9 @@
 DROP DATABASE europeancodeweek;
 
-CREATE DATABASE IF NOT EXISTS europeancodeweek;
+CREATE DATABASE IF NOT EXISTS europeancodeweek DEFAULT CHARACTER SET utf8  DEFAULT COLLATE utf8_general_ci;
+
+USE europeancodeweek;
 
 GRANT ALL PRIVILEGES ON europeancodeweek.* TO root@localhost IDENTIFIED BY 'mysql';
 
 FLUSH PRIVILEGES;
-
-USE europeancodeweek;
