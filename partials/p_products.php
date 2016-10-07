48bf2c0c968a8e50151bed6fc3974c55e64e3b9a
Danijel Vrgoc <danijel.vrgoc@inchoo.net>
2016-10-07 22:11:34 +0200

    added tab panes for products

--- partials/p_products.php
+++ partials/p_products.php
@@ -1,1 +1,19 @@
-<?php echo "Hello from articles.php" ?>
+<main class="col-sm-8">
+	<section>
+		<!-- Nav tabs -->
+		<ul class="nav nav-tabs" role="tablist">
+			<li role="presentation" class="active"><a href="#list" aria-controls="list" role="tab" data-toggle="tab">List</a></li>
+			<li role="presentation"><a href="#new" aria-controls="new" role="tab" data-toggle="tab">New</a></li>
+			<li role="presentation"><a href="#edit" aria-controls="edit" role="tab" data-toggle="tab">Edit</a></li>
+			<li role="presentation"><a href="#delete" aria-controls="delete" role="tab" data-toggle="tab">Delete</a></li>
+		</ul>
+
+		<!-- Tab panes -->
+		<div class="tab-content">
+			<div role="tabpanel" class="tab-pane active" id="list">1</div>
+			<div role="tabpanel" class="tab-pane" id="new">2</div>
+			<div role="tabpanel" class="tab-pane" id="edit">3</div>
+			<div role="tabpanel" class="tab-pane" id="delete">4</div>
+		</div>
+	</section>
+</main>
