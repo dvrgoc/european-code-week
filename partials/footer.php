6b47bdddf8aea76137c3762850419d651758713c
Danijel Vrgoc <danijel.vrgoc@inchoo.net>
2016-10-08 00:59:13 +0200

    styled footer

--- partials/footer.php
+++ partials/footer.php
@@ -3,8 +3,22 @@
 
 <footer class="row">
 	<div class="container">
-		<p>European Code Week, Osijek, October 19th 2016</p>
-		<p><a href="mailto:danijel.vrgoc@inchoo.net">danijel.vrgoc@inchoo.net</a></p>
+		<p class="col-xs-12 col-sm-6 col-md-3">
+			<span class="glyphicon glyphicon-education"></span>
+			<a href="http://codeweek.eu/" title="European Code Week">European Code Week</a></span>
+		</p>
+		<p class="col-xs-12 col-sm-6 col-md-3">
+			<span class="glyphicon glyphicon-globe"></span>
+			<a href="https://www.google.hr/maps/place/Osijek/@45.5462461,18.5487771,11z/data=!3m1!4b1!4m5!3m4!1s0x475ce7a869728075:0x5b8c725621a41195!8m2!3d45.5549624!4d18.6955144?hl=en" title="Osijek, Croatia">Osijek, Croatia</a>
+		</p>
+		<p class="col-xs-12 col-sm-6 col-md-3">
+			<span class="glyphicon glyphicon-calendar"></span>
+			<span>October 19th 2016</span>
+		</p>
+		<p class="col-xs-12 col-sm-6 col-md-3">
+			<span class="glyphicon glyphicon-envelope"></span>
+			<a href="mailto:danijel.vrgoc@inchoo.net">danijel.vrgoc@inchoo.net</a>
+		</p>
 	</div>
 </footer>
 
